c5b47bb35747fb9ea1df9e1af53defcb28a3a5b7
RyuDouZx <161895864+RyuDouZx@users.noreply.github.com>
2024-04-03 10:00:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+let conexion = mysql.createConnection({
+    host: "localhost",
+    database: "zxshop",
+    user: "root",
+    password: ""
+})
+
+// Middleware para servir archivos estáticos desde el directorio 'public'
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Ruta para servir index.html cuando se acceda al directorio raíz '/'
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Ruta para servir login.html cuando se acceda a '/login'
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + "/login.html");
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log("Servidor Express escuchando en http://localhost:"+port);
+});
+
+// micodigogogogo
+
+app.post("/validar", function(req,res){
+    const datos = req.body;
+    let id = datos.id;
+    let nombres = datos.nombres;
+    let apellidos = datos.apellidos;
+    let correo = datos.correo;
+    let contrasenia = datos.contrasenia
+    
+    let consulta = "INSERT INTO tabla_usuarios (id, nombres, apellidos, correo, contrasenia) VALUE ('"+id+"', '"+nombres+"', '"+apellidos+"', '"+correo+"', '"+contrasenia+"')"
+    conexion.query(consulta, function(error){
+        if(error){
+            console.log("algo salió mal")
+        }else{
+            console.log("se almaceno correctamente")
+        }
+    })
+})
---
+++ public/main.js
@@ -0,0 +1,46 @@
+console.log("hola mundo")
+const cloud = document.getElementById("logg");
+const barraLateral = document.querySelector(".barra-lateral");
+const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+document.getElementById('crear_cuentaa').addEventListener('click', function() {
+    window.location.href = '/login';
+});
+
+menu.addEventListener("click",()=>{
+    barraLateral.classList.toggle("max-barra-lateral");
+    if(barraLateral.classList.contains("max-barra-lateral")){
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block";
+    }
+    else{
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";
+    }
+    if(window.innerWidth<=320){
+        barraLateral.classList.add("mini-barra-lateral");
+        main.classList.add("min-main");
+        spans.forEach((span)=>{
+            span.classList.add("oculto");
+        })
+    }
+});
+
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    body.classList.toggle("prendido");
+    circulo.classList.toggle("prendido");
+});
+
+cloud.addEventListener("click",()=>{
+    barraLateral.classList.toggle("mini-barra-lateral");
+    spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+    });
+});
+
